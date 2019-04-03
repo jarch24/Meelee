504bfdaa6b63a1df92430cc566e54c9a2335389c
jgiri <jgiri@DMINBNOI19025>
2019-04-03 10:19:25 +0530

    Exception Page Changed

--- Meelee/Startup.cs
+++ Meelee/Startup.cs
@@ -40,15 +40,15 @@ namespace Meelee
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
